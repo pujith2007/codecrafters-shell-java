49ae534ef308b34653087d50f7751a2dbe8fd622
srivasthav sarathy <raghunandan@MacBook-Pro-9.local>
2026-06-19 20:02:10 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,11 @@ public class Main {
 
             if (input.equals("exit")) {
                 break;
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
             }
-
-            System.out.println(input + ": command not found");
         }
     }
 }
